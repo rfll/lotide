de7e1936c55ff7ec3ef78d218e583eb8d38a98a9
Ryan Fortin <ryanwfortin@gmail.com>
2022-07-25 23:41:49 +0000

    Function to replace console.assert

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍🏻 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("✋🏻 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(7, 3);
