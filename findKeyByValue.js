f53d1039080511571f3da335529019c2f1cd2390
Ryan Fortin <ryanwfortin@gmail.com>
2022-07-28 05:29:43 +0000

    Loop through object to compare two keys

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍🏻 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✋🏻 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(randomKey, randomValue) {
+  
+  let something = undefined;
+
+  for (let key in randomKey) {
+    if (randomKey[key] === randomValue) {
+      something = key;
+    }
+  }
+  return something;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
