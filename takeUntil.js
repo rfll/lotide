5bfc29fcdd8e7c5ca62beb89599e2c4d9c0d266a
Ryan Fortin <ryanwfortin@gmail.com>
2022-07-29 01:37:32 +0000

    Alter array based on condition of callback function

---
+++ takeUntil.js
@@ -0,0 +1,73 @@
+const eqArrays = function(arrOne, arrTwo) {
+  // Are the arrays the same length? If not, false
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  // Compare the arrays
+  for (let i = 0; i < arrOne.length; i++) {
+  // Are the array values the same? If not, false
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log("These 2 arrays are equal");
+  } else {
+    console.log("These 2 arrays are not equal");
+  }
+};
+
+
+
+
+const takeUntil = function(array, callback) {
+  // Function should...
+  // Take in an array as input paramter "array"
+  // Loop through array
+
+  let arr = [];
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (callback(array[i])) {
+      
+      break;
+
+    } else {
+        
+      arr.push(array[i]);
+    }
+    // console.log(arr);
+  }
+
+  // Break loop when truthy value is found
+  // callback parameter used to callback anonymous function "x"
+  
+  return arr;
+
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+// Expected output
+
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
