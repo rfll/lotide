01222b8ed22228b19058a7350d67d88667e9c0db
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-01 20:50:23 +0000

    Refactored assertEqual file to export to a test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,10 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍🏻 Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍🏻 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("✋🏻 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`✋🏻 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual(7, 3);
+
+module.exports = assertEqual;
