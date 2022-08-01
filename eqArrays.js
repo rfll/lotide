7528b5808f4b76490a6930fc7c8379766328d968
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-01 21:15:43 +0000

    Refactored code to export to test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,17 @@
 const eqArrays = function(arrOne, arrTwo) {
 // Are the arrays the same length? If not, false
-  if (arrOne.length !== arrTwo.length){
+  if (arrOne.length !== arrTwo.length) {
     return false;
   }
-// Compare the arrays
+  // Compare the arrays
   for (let i = 0; i < arrOne.length; i++) {
-  // Are the array values the same? If not, false  
+  // Are the array values the same? If not, false
     if (arrOne[i] !== arrTwo[i]) {
       return false;
-  }
+    }
   }
   return true;
-}
+};
 
 
 module.exports = eqArrays;
